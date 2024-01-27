b4537088ded76db2817c510af348c598a557a5db
Barış SAVAŞ <barissavas17@gmail.com>
2024-01-27 22:53:36 +0300

    feat: logs removed from the code

--- delete.go
+++ delete.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -9,18 +11,23 @@ import (
 func DeleteTodo(c *fiber.Ctx) error {
 	todoID, err := primitive.ObjectIDFromHex(c.Params("id"))
 	if err != nil {
-		return c.SendStatus(400)
+		fmt.Println("Error parsing todoID:", err)
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
 	query := bson.D{{Key: "_id", Value: todoID}}
+
 	result, err := mg.Db.Collection("Todo-1").DeleteOne(c.Context(), &query)
-
 	if err != nil {
-		return c.SendStatus(500)
+		fmt.Println("Error deleting document:", err)
+		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	if result.DeletedCount < 1 {
-		return c.SendStatus(404)
+
+	if result.DeletedCount == 0 {
+		fmt.Println("Document not found for deletion")
+		return c.SendStatus(fiber.StatusNotFound)
 	}
-	return c.Status(200).JSON("record deleted")
+
+	return c.Status(fiber.StatusOK).JSON("record deleted")
 }
--- getTodoById.go
+++ getTodoById.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+
 func GetTodo(c *fiber.Ctx) error {
-	todoID, err := primitive.ObjectIDFromHex(c.Params("id"))
-	if err != nil {
-		return c.SendStatus(400)
-	}
+    todoID, err := primitive.ObjectIDFromHex(c.Params("id"))
+    if err != nil {
+        fmt.Println("Error parsing ID:", err)
+        return c.SendStatus(fiber.StatusBadRequest)
+    }
 
-	query := bson.D{{Key: "_id", Value: todoID}}
-	result := mg.Db.Collection("Todo-1").FindOne(c.Context(), &query)
+    query := bson.D{{Key: "_id", Value: todoID}}
 
-	var todo Todo
-	err = result.Decode(&todo)
-	if err != nil {
-		return c.SendStatus(500)
-	}
+    result := mg.Db.Collection("Todo-1").FindOne(c.Context(), &query)
+    if result.Err() != nil {
+        if result.Err() == mongo.ErrNoDocuments {
+            fmt.Println("No document found")
+            return c.SendStatus(fiber.StatusNotFound)
+        }
+        fmt.Println("Error during FindOne:", result.Err())
+        return c.SendStatus(fiber.StatusInternalServerError)
+    }
 
-	return c.Status(200).JSON(todo)
+    var todo Todo
+    err = result.Decode(&todo)
+    if err != nil {
+        fmt.Println("Error decoding document:", err)
+        return c.SendStatus(fiber.StatusInternalServerError)
+    }
 
+    return c.Status(fiber.StatusOK).JSON(todo)
 }
